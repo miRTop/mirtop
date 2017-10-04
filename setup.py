cd4d5bccd67d45af9dedd093042324f2ff129e9b
Lorena Pantano <lorena.pantano@gmail.com>
2017-10-04 19:20:52 -0400

    push to stable version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 
 setup(name='mirtop',
-      version='0.1.5a',
+      version='0.1.6',
       description='Small RNA-seq annotation',
       long_description=readme(),
       classifiers=[
