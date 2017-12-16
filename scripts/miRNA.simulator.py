9ea6e301f8176219794db7482752e0472c95e392
Lorena Pantano <lorena.pantano@gmail.com>
2017-12-15 19:15:41 -0500

    Improve simulator

--- scripts/miRNA.simulator.py
+++ scripts/miRNA.simulator.py
@@ -5,144 +5,165 @@ import re
 import random
 import numpy
 
-def createMirna(string):
-        cols = string.split("[")
-        idx = 0
-        obj=mirna()
-        for c in cols[1:]:
-                idx += 1
-                c=c.replace("]","")
-                p=c.split(":")
-                chrom=p[0]
-                p=p[1].split("-")
-                start=int(p[0])
-                end=int(p[1])
-                if idx == 1:
-                        obj.addp5(chrom,start,end)
-                else:
-                        obj.addp3(chrom,start,end)
-        return obj
+from mirtop.mirna import fasta
+from mirtop.mirna import mapper
+from mirtop.mirna import realign
+from mirtop.gff import body, header
+import mirtop.libs.logger as mylog
+logger = mylog.getLogger(__name__)
 
-class pos:
-	def __init__(self,n,s,e):
-		self.s=s
-		self.e=e
-		self.id=n
+def write_collapse_fastq(reads, out_fn):
+    idx = 0
+    with open(out_fn, 'a') as outh:
+        for r in reads:
+            idx += 1
+            print >>outh, ">name%s_x%s" % (idx, r[1])
+            print >>outh, r[0]
 
-class mirna:
-	def __init__(self):
-		self.p5=0
-		self.p3=0
-	def addp5(self,n,s,e):
-		#print "adding p5"
-		self.p5=pos(n,s,e)
-		#print self.p5
-	def addp3(self,n,s,e):
-		self.p3=pos(n,s,e)
+def write_fastq(reads, out_fn):
+    idx = 0
+    with open(out_fn, 'a') as outh:
+        for r in reads:
+            idx += 1
+            print >>outh, "@name_read:%s" % idx
+            print >>outh, r
+            print >>outh, "+"
+            print >>outh, "I" * len(r)
 
+def create_read(read, count, adapter="TGGAATTCTCGGGTGCCAAGGAACTC", size=36):
+    reads = list()
+    for i in  range(0, count):
+        rest = size - len(read)
+        part = adapter[:rest]
+        reads.append(read + part)
+    return reads
 
-def create_iso(listmirna, name, seq, fout, fqout):
-    mir=listmirna[name].p5
-    if mir!=0 and name.find(species)>=0:
-        for rand in range(int(options.numsim)):
-            randS=random.randint(mir.s-2,mir.s+2)+1
-            randE=random.randint(mir.e-2,mir.e+2)+1
-            if randS<1:
-                    randS=1
-            if randE>mir.e:
-                    randE=mir.e-1
-            randSeq=seq[randS-1:randE]
-            randName=name+"_"+mir.id+"_"+str(randS)+":"+str(randE)
-            randName=randName+"_"+str(randS-mir.s)+":"+str(randE-mir.e)
-            isMut=random.randint(0,3)
-            mut_tag = "_mut:null"
-            if isMut==3:
-                ntMut=random.randint(0,3)
-                posMut=random.randint(0,len(randSeq)-1)
-                tempList=list(randSeq)
-                if tempList[posMut] == nt[ntMut]:
-                    ntMut -= 1
-                    if ntMut < 0 :
-                        ntMut +=2
-                tempList[posMut]=nt[ntMut]
-                randSeq=''.join(tempList)
-                mut_tag="_mut:"+str(posMut+1)+nt[ntMut]
-            randName+=mut_tag
-            isAdd=random.randint(0,2)
-            add_tag = "_add:null"
-            if isAdd==2:
-                posAdd=random.randint(1,3)
-                randAdd=""
-                add_tag="_add:"
-                for numadd in range(posAdd):
-                        ntAdd=random.randint(0,1)
-                        randSeq+=nt[ntAdd]
-                        add_tag+=nt[ntAdd]
-            randName+=add_tag
-            exp = ""
-            if not data.has_key(randSeq):
-                if options.exp:
-                    trial =random.randint(1, 100)
-                    p = random.randint(1, 50) / 50.0
-                    exp = "_x%s" % numpy.random.negative_binomial(trial, p, 1)[0]
-                print >>fqout, "@%s%s" % (randName, exp)
-                print >>fqout, "%s" % randSeq
-                print >>fqout, "+\n%s" % "".join(["I"] * len(randSeq))
-                print >>fout, ">%s%s" % (randName, exp)
-                print >>fout, "%s" % randSeq
-                data[randSeq]=1
+def variation(info, seq):
+    randS = random.randint(info[0] - 2, info[0] + 2) + 1
+    randE = random.randint(info[1] - 1, info[1] + 2) + 1
+    if randS < 1:
+        randS = 1
+    if randE > len(seq):
+        randE = info[1] - 1
+    randSeq = seq[randS:randE]
+    t5Lab = ""
+    t5Lab = seq[randS:info[0]] if randS < info[0] else t5Lab
+    t5Lab = seq[info[0]:randS].lower() if randS > info[0] else t5Lab
+    t3Lab = ""
+    t3Lab = seq[randE:info[1] + 1].lower() if randE < info[1] + 1 else t3Lab
+    t3Lab = seq[info[1] + 1:randE] if randE > info[1] + 1 else t3Lab
+    # mutation
+    isMut = random.randint(0, 10)
+    mutLab = []
+    if isMut == 3:
+        ntMut = random.randint(0, 3)
+        posMut = random.randint(0, len(randSeq) - 1)
+        if not randSeq[posMut] == nt[ntMut]:
+            temp = list(randSeq)
+            mutLab = [[posMut, nt[ntMut], randSeq[posMut]]]
+            temp[posMut] = nt[ntMut]
+            randSeq = "".join(temp)
 
+    # addition
+    isAdd = random.randint(0, 3)
+    addTag = ""
+    if isAdd == 2:
+        posAdd = random.randint(1, 3)
+        for numadd in range(posAdd):
+            ntAdd = random.randint(0, 1)
+            print [randSeq, seq[randS + len(randSeq)]]
+            if nt[ntAdd] == seq[randS + len(randSeq)]:
+                ntAdd = 1 if ntAdd == 0 else 0
+            randSeq += nt[ntAdd]
+            addTag += nt[ntAdd]
+            print [randSeq, randE, info[1]]
+    return [randSeq, randS, t5Lab, t3Lab, mutLab, addTag]
 
-usagetxt = "usage: %prog  -f precurso.fa -m miRNA.str -n 10 -s hsa"
+def create_iso(name, mir, seq, numsim, exp):
+    data = dict()
+    reads = dict()
+    full_read = list()
+    clean_read = list()
+    seen = set()
+    for mirna in mir[name]:
+        info = mir[name][mirna]
+        mirSeq = seq[info[0]:info[1] + 1]
+        for rand in range(int(numsim)):
+             # expression
+            e = 1
+            if exp:
+                trial = random.randint(1, 100)
+                p = random.randint(1, 50) / 50.0
+                e = numpy.random.negative_binomial(trial, p, 1)[0]
+            iso = realign.isomir()
+            randSeq, iso.start, iso.t5, iso.t3, iso.subs, iso.add = variation(info, seq)
+            if randSeq in seen:
+                continue
+            seen.add(randSeq)
+            iso.end = iso.start + len(randSeq)
+            aln = realign.align(randSeq, seq[iso.start:iso.end])
+            iso.cigar = realign.make_cigar(aln[0], aln[1])
+            iso.mirna = mirna
+            query_name = "%s.%s.%s" % (mirna, iso.format_id("."), randSeq)
+            reads[query_name] = realign.hits()
+            reads[query_name].set_sequence(randSeq)
+            reads[query_name].counts = e
+            reads[query_name].set_precursor(name, iso)
+            full_read.extend(create_read(randSeq, e))
+            clean_read.append([randSeq, e,])
+            # print [randSeq, mutLab, addTag, t5Lab, t3Lab, mirSeq]
+            # data[randSeq] = [exp, iso] # create real object used in code to generate GFF
+    write_fastq(full_read, full_fq)
+    write_collapse_fastq(clean_read, clean_fq)
+    gff = body.create(reads, "miRBase21", "sim1")
+    return gff
+
+def _write(lines, header, fn):
+    out_handle = open(fn, 'w')
+    print >>out_handle, header
+    for m in lines:
+        for s in sorted(lines[m].keys()):
+            for hit in lines[m][s]:
+                print >>out_handle, hit[4]
+    out_handle.close()
+
+usagetxt = "usage: %prog  --fa precurso.fa --gtf miRNA.gtf -n 10"
 
 parser = OptionParser(usage=usagetxt, version="%prog 1.0")
-parser.add_option("-f", "--fa", dest="fasta",
+parser.add_option("--fa",
                   help="", metavar="FILE")
-parser.add_option("-m", "--ma", dest="strfile",
+parser.add_option("--gtf",
                                 help="", metavar="FILE")
 parser.add_option("-n", "--num", dest="numsim",
                                 help="")
-parser.add_option("-s", "--species", dest="species",
-                                help="", metavar="FILE")
 parser.add_option("-e", "--exp", dest="exp", action="store_true",
                                 help="give expression", default=False)
 parser.add_option("-p", "--prefix", help="output name")
+parser.add_option("--seed", help="set up seed for reproducibility.", default = None)
 
-
-if len(sys.argv)<4:
-    parser.print_help()
-    sys.exit(1)
 
 (options, args) = parser.parse_args()
-species=options.species
-out_fa = "%s.fa" % options.prefix
-out_fq = "%s.fq" % options.prefix
 
-pos_mirna=open(options.strfile,'r')
-listmirna={}
-for line in pos_mirna:
-        if line.find("[")>0:
-                line = line.strip()
-                name = line.split(" ")
-                name = name[0].replace(">","")
-                listmirna[name]=createMirna(line)
-pos_mirna.close()
+if options.seed:
+    random.seed(options.seed)
 
-data = {}
-fas = open(options.fasta, 'r')
-fout = open(out_fa, 'w')
-fqout = open(out_fq, 'w')
-name = ""
-seq = ""
+full_fq = "%s_full.fq" % options.prefix
+clean_fq = "%s_clean.fq" % options.prefix
+out_gff = "%s.gff" % options.prefix
+if os.path.exists(full_fq):
+    os.remove(full_fq)
+if os.path.exists(clean_fq):
+    os.remove(clean_fq)
+
+pre = fasta.read_precursor(options.fa, "")
+mir = mapper.read_gtf_to_precursor(options.gtf)
+
 nt = ['A', 'T', 'G', 'C']
-for line in fas:
-    line = line.strip()
-    if line.startswith(">"):
-        if (len(name)!=0):
-            create_iso(listmirna, name, seq, fout, fqout)
-        name = line.split(" ")
-        name = name[0].replace(">","")
-        seq=""
-    else:
-        seq+=line.replace("U","T")
-create_iso(listmirna, name, seq, fout, fqout)
+gffs = dict()
+h = header.create(["sampleX"], "miRBase1", "")
+for precursor in pre:
+    seq = pre[precursor]
+    gffs.update(create_iso(precursor, mir, seq, options.numsim, options.exp))
+
+
+_write(gffs, h, out_gff)
