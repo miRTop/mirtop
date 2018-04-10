69116accd74613cbcf6602db5b87c52d4c66a255
AlisR <rafaalis@gmail.com>
2018-04-10 11:47:33 +0200

    Update import_gff3.py

--- scripts/import_gff3.py
+++ scripts/import_gff3.py
@@ -1,21 +1,26 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd
-from collections import defaultdict
+import numpy as np
+
 
 """
 Function that loads a gff file into a pandas dataframe
 """
-def loadfile(filename,verbose):
+def loadfile(filename,verbose,zero=False):
         
     try:
         if verbose==True:
             print 'Loading', filename
         # obtaning sample names and number from 3rd line in header
         with open(filename) as f:
-            f.readline()
-            f.readline()
-            sample_names = f.readline().split()[2].split(',')
+            rowfile=f.readline()
+            while True:
+                if rowfile.startswith('## COLDATA'):
+                    sample_names=rowfile.split()[2].split(',')
+                    break
+                else:
+                    rowfile=f.readline()
         sample_number = len(sample_names)
         if verbose==True:
             print '--------------------------------------'
@@ -62,14 +67,33 @@ def loadfile(filename,verbose):
             list_expression.append(atr_data.values[row,expression_colindex].split(','))
         sample_names=['Expression_' +x for x in sample_names]
         
-        expression_data = pd.DataFrame(list_expression, columns=sample_names)
+        expression_data = pd.DataFrame(list_expression, columns=sample_names)      
+        
         
         atr_data=atr_data.drop('Expression',axis=1) #Remove the expression column
         atr_data=atr_data.join(expression_data)
     
         # Joining the body and attributes dataframes
         data = body_data.join(atr_data)
-    
+        
+        #Unlisting the variant column
+        tempframe=data[data.type=='isomiR']
+        list_variants_present=[]
+        for row in tempframe.itertuples():      
+            actual_variant=tempframe.loc[row.Index,'Variant'].split(',')
+            for i in range(len(actual_variant)):
+                if actual_variant[i] not in list_variants_present:
+                    list_variants_present.append(actual_variant[i])
+                    
+                    
+        
+        for var in list_variants_present:
+            for row in data.itertuples():
+                if var in data.loc[row.Index,'Variant']:
+                    #print var, data.loc[row.Index,'Variant']
+                    data.at[row.Index,var]=1
+                else:
+                    data.at[row.Index,var]=np.nan    
         return data
     except:
         print 'Error loading the file'
@@ -79,27 +103,26 @@ def loadfile(filename,verbose):
 Function that check the header then load a gff file and check the content
 Returns the dataframe if the format is ok, false if not
 """
-def load_gff3(filename):
+def load_check_gff3(filename):
     try:
-        Error = False     
+        Error = False
+        coldata_found=False
         # Checking the format file 
         # Header and 1st data row
-        header_rows=0
         with open(filename) as file:
-            rowfile=file.readline()
-            header_rows=header_rows+1
-
+            rowfile=file.readline()           
             while True:
-                if rowfile.startswith('#'):
+                if rowfile.startswith('##'):
+                    if rowfile.startswith("## COLDATA"):
+                         coldata_found=True
                     rowfile=file.readline()
-                    header_rows=header_rows+1
                 else:
                     data_1=rowfile.split('\t')
-
-                    if header_rows!=4:
-                        Error = True
-                        print 'Bad header' 
                     break
+        if coldata_found==False:
+            print 'No COLDATA, bad header'
+            return False
+        
         #Number of columns without breaking down attributes column 
         if len(data_1) > 9:
             Error=True
@@ -131,17 +154,17 @@ def load_gff3(filename):
             # Labels in type column
             if dataframe.loc[i, 'type'] not in ['ref_miRNA', 'isomiR']:
                 Error = True
-                print('line', i, 'bad type error')
+                print'line', i, 'bad type error'
     
             # start<end
             if dataframe.loc[i, 'start'] >= dataframe.loc[i, 'end']:
                 Error = True
-                print('line', i, 'start >=end error')
+                print 'line', i, 'start >=end error'
     
             # Strand + or -
             if dataframe.loc[i, 'strand'] not in ['+', '-']:
                 Error = True
-                print('line', i, 'bad strand error')
+                print 'line', i, 'bad strand error'
             # Variant checking
             possible_variant=['iso_5p','iso_3p','iso_add','iso_snp_seed','iso_snp_central_offset','iso_snp_central',
                   'iso_central_supp','iso_snp_central_supp','iso_snp']
@@ -157,6 +180,7 @@ def load_gff3(filename):
                     if variant_i[var].split(':')[0] not in possible_variant:
                         Error = True
                         print 'Variant error', variant_i[0].split(':')[0], 'line', i
+        #dataframe=dataframe.drop('Variant',index=1) #Remove the Variant column
         #Checking expression data
         expression_cols=[col for col in dataframe.columns if 'Expression_' in col]         
         for col in expression_cols:
@@ -166,6 +190,7 @@ def load_gff3(filename):
                     print 'Expression count error line',i
                     Error= True
             dataframe[col]=dataframe[col].astype(int) #setting the datatype of counts
+            dataframe[col]=dataframe[col].replace(0,np.nan) #Setting 0 reads to NaN
         if 'Filter' in dataframe.columns:         
             for i in range(dataframe.shape[0]):
                 if dataframe.loc[i, 'Filter']!='Pass':
@@ -173,6 +198,7 @@ def load_gff3(filename):
         if Error:
             print 'File format error'
             return False
+        dataframe=dataframe.drop('Variant',axis=1) #Remove the Variant column
         print '--------------------------------------'
         print dataframe.dtypes
         print '--------------------------------------'
@@ -185,163 +211,13 @@ def load_gff3(filename):
 
 
 
-file='/home/rafa/mirtop_project/Scripts/blood.gff3'
+#file='/home/rafa/mirtop_project/Scripts/blood.gff3'
+#file='/home/rafa/mirtop_project/Scripts/blood2.gff3'
+file='/home/rafa/mirtop_project/Aligments/blood/blood2/13_plasma-1-TruSeq_fastq-mirbase-ready.gff3'
+
 #file='/home/rafa/mirtop_project/Scripts/gff_examples/2uid_missing.gff'
 #file='/home/rafa/mirtop_project/Scripts/gff_examples/coldata_missing.gff'
 #file='/home/rafa/mirtop_project/Scripts/gff_examples/missing_filter_type.gff'
 #file='/home/rafa/mirtop_project/Scripts/gff_examples/3wrong_type.gff'
 
-filedata = load_gff3(file)
-
-
-
-# Adquiring the sample names in a dataframe containing a gff3 file
-def samplenames(dataframe):
-    return [col for col in dataframe.columns if 'Expression_' in col]
-
-# Determining the list of variants presente in a dataframe containing a gff3 file
-def listvariants(filedata): 
-    dataframe=filedata[filedata.type=='isomiR']
-    list_variants_present=[]
-    for index, row in dataframe.iterrows():      
-        actual_variant=dataframe.loc[index,'Variant'].split(',')
-        for i in range(len(actual_variant)):
-            if actual_variant[i] not in list_variants_present:
-                list_variants_present.append(actual_variant[i])
-    return list_variants_present
-
-# Number smallRNA by sample
-def number_smallRNA_by_sample(dataframe):
-    sample_names=samplenames(dataframe)
-    print 'Number smallRNA by sample'
-    print '--------------------------------------'
-    total_number_dict={}
-    for col in sample_names:
-        total_number_dict[col]=dataframe[dataframe[col]>0][col].count()
-        print  total_number_dict[col], col
-    print '--------------------------------------'
-    return total_number_dict
-
-# Number of miRNA or isomiR in each sample
-def number_smallRNA_by_type_by_sample(dataframe):
-    sample_names=samplenames(dataframe)
-    result=pd.DataFrame(index=sample_names, columns=['miRNA','isomiR'])
-    for col in sample_names:
-        result.at[col,'miRNA']=dataframe[(dataframe.type=='ref_miRNA')&(dataframe[col]>0)][col].count()
-        result.at[col,'isomiR']=dataframe[(dataframe.type=='isomiR')&(dataframe[col]>0)][col].count()
-    print result
-    return result
-
-# Number of isomir by type of modification
-def number_isomir_by_type_by_sample(filedata):
-    dataframe=filedata[filedata.type=='isomiR']
-    sample_names=samplenames(dataframe)
-    list_variants_present=listvariants(dataframe)
-    result=pd.DataFrame(index=sample_names, columns=list_variants_present)
-    result=result.fillna(0)
-    for col in sample_names:
-        for index,row in dataframe.iterrows():
-            actual_variant=[]
-            actual_variant=dataframe.loc[index,'Variant'].split(',')
-            for i in range(len(actual_variant)):
-                var=actual_variant[i]
-                if dataframe.loc[index,col]!=0:
-                    result.at[col,var]+=1
-    print result
-    return result
-
-
-for col in samplenames(filedata):
-    print filedata[filedata[col]>0][col].describe()
-
-# Total counts by sample
-def total_counts_by_sample(dataframe):
-    sample_names=samplenames(dataframe)
-    print 'Total counts by sample'
-    print '--------------------------------------'
-    total_counts_dict={}
-    for col in sample_names:
-        total_counts_dict[col]=dataframe[col].sum()
-        print  total_counts_dict[col], col
-    print '--------------------------------------'
-    return total_counts_dict
-
-# Counts by type miRNA or isomiR
-def counts_by_type_by_sample(dataframe):
-    sample_names=samplenames(dataframe)
-    result=pd.DataFrame(index=sample_names, columns=['miRNA','isomiR'])
-    for col in sample_names:
-        result.at[col,'miRNA']=dataframe.groupby('type')[col].sum()['ref_miRNA']
-        result.at[col,'isomiR']=dataframe.groupby('type')[col].sum()['isomiR']
-    print result
-    return result
-
-# Counts by type of isomiR
-def counts_by_isomir_type(filedata):
-    dataframe=filedata[filedata.type=='isomiR']
-    sample_names=samplenames(dataframe)
-    list_variants_present=listvariants(dataframe)
-    result=pd.DataFrame(index=sample_names, columns=list_variants_present)
-    result=result.fillna(0)
-    for col in sample_names:
-        for index,row in dataframe.iterrows():
-            actual_variant=[]
-            actual_variant=dataframe.loc[index,'Variant'].split(',')
-            for i in range(len(actual_variant)):
-                var=actual_variant[i]
-                result.at[col,var]+=dataframe.loc[index,col]
-    print result
-    return result
-
-
-
-number_smallRNA_by_sample(filedata)
-number_smallRNA_by_type_by_sample(filedata)
-number_isomir_by_type_by_sample(filedata)
-total_counts_by_sample(filedata)
-counts_by_type_by_sample(filedata)
-counts_by_isomir_type(filedata)
-
-# Plots
-import matplotlib.pyplot as plt
-import numpy as np
-list_variants_present=listvariants(filedata)
-list_variants_present.sort()
-
-# Counts of all variants in all samples
-counts_type_isomir=counts_by_isomir_type(filedata)
-counts_type_isomir=counts_type_isomir[list_variants_present]
-fig=counts_type_isomir.T.plot(kind='line',title='Counts all modifications')
-fig.set_xticks(np.arange(0,23,1))
-fig.set_xticklabels(counts_type_isomir.columns, rotation=90)
-
-# counts of 3' modifications in all samples
-list_3=['iso_3p:+4','iso_3p:+3','iso_3p:+2','iso_3p:+1','iso_3p:-1','iso_3p:-2','iso_3p:-3','iso_3p:-4']
-fig=counts_type_isomir[list_3].T.plot(kind='line',title='Counts with 3 end modifications')
-fig.set_xticks(np.arange(0,8,1))
-fig.set_xticklabels(list_3, rotation=90)
-
-# counts of 5' modifications in all samples
-list_5=['iso_5p:+3','iso_5p:+2','iso_5p:+1','iso_5p:-1','iso_5p:-2','iso_5p:-3']
-fig=counts_type_isomir[list_5].T.plot(kind='line',title='Counts with 5 end modifications')
-fig.set_xticks(np.arange(0,6,1))
-fig.set_xticklabels(list_5, rotation=90)
-
-# Number of isomir with all variants in all samples
-num_isomir_type=number_isomir_by_type_by_sample(filedata)
-num_isomir_type=num_isomir_type[list_variants_present]
-fig=num_isomir_type.T.plot(kind='line',title='Number of IsomiR by all modifications')
-fig.set_xticks(np.arange(0,23,1))
-fig.set_xticklabels(num_isomir_type.columns, rotation=90)
-
-# Number of isomir with 3' modifications in all samples
-list_3=['iso_3p:+4','iso_3p:+3','iso_3p:+2','iso_3p:+1','iso_3p:-1','iso_3p:-2','iso_3p:-3','iso_3p:-4']
-fig=num_isomir_type[list_3].T.plot(kind='line',title='Number of IsomiR with 3 end modifications')
-fig.set_xticks(np.arange(0,8,1))
-fig.set_xticklabels(list_3, rotation=90)
-
-# Number of isomir with 5' modifications in all samples
-list_5=['iso_5p:+3','iso_5p:+2','iso_5p:+1','iso_5p:-1','iso_5p:-2','iso_5p:-3']
-fig=num_isomir_type[list_5].T.plot(kind='line',title='Number of IsomiR with 5 end modifications')
-fig.set_xticks(np.arange(0,6,1))
-fig.set_xticklabels(list_5, rotation=90)
+filedata = load_check_gff3(file)
