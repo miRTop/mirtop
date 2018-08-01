9057c00a270049296735928d8385ae2e0a828541
Lorena Pantano <lorena.pantano@gmail.com>
2018-08-01 18:13:00 -0400

    sync gff version with mirGFF3 repo

--- mirtop/gff/gff_versions.py
+++ mirtop/gff/gff_versions.py
@@ -1,4 +1,4 @@
-current = "0.9"
-GFFv = {'0.9': ["iso_5p", "iso_3p", "iso_add", "iso_snp_seed",
+current = "1.0"
+GFFv = {'1.0': ["iso_5p", "iso_3p", "iso_add", "iso_snp_seed",
                 "iso_snp_central_offset", "iso_snp_central",
                 "iso_snp_central_supp", "iso_snp", "NA"]}
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '0.4.0a'
+version = '0.4.1a'
 
 
 def readme():
