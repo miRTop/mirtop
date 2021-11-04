007f7b5bfca1da7cf66b20b7c105edd57acd7007
Lorena Pantano <lorena.pantano@gmail.com>
2021-11-04 11:54:02 -0400

    Fix issue #69

--- test/test_functions.py
+++ test/test_functions.py
@@ -312,6 +312,7 @@ class FunctionsTest(unittest.TestCase):
                                   "iso_5p:+2,iso_3p:-2,iso_add3p:2")
         if res:
             raise ValueError("Wrong alignment for test 8 %s" % res)
+        
 
     @attr(alignment=True)
     def test_alignment(self):
@@ -528,3 +529,12 @@ class FunctionsTest(unittest.TestCase):
         from mirtop.gff.update import update_file
         print("\n")
         update_file("data/examples/versions/version1.0.gff", None)
+
+    @attr(error69=True)
+    def test_error69(self):
+        from mirtop.bam.filter import tune
+        v = tune("CTTATCAGATTGTATTGTAATT", 
+                 "TACATCGGCCATTATAATACAACCTGATAAGTGTTATAGCACTTATCAGATTGTATTGTAATTGTCTGTGTANNNNNNNNNNNN", 
+                 41, [(0, 22)])
+        if v[2] != "22M":
+            raise ValueError("Issue 69 is back. Variantion not detected correctly.")
